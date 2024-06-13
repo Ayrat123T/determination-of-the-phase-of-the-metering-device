868c177da76c0676ee8ed2f6eaff21c6544b6755
Ayrat Tuityarov <114942559+Ayrat123T@users.noreply.github.com>
2024-06-13 16:56:47 +0300

    Update input_reader.cpp

--- determination_of_the_phase_of_the_metering_device/src/input_reader.cpp
+++ determination_of_the_phase_of_the_metering_device/src/input_reader.cpp
@@ -1,1 +1,19 @@
 #include "input_reader.h"
+
+std::vector<std::string_view> detailMeterVoltageProfilesIO::SplitIntoWordsBySemicolon(std::string_view str) {
+    std::vector<std::string_view> result;
+    const int64_t pos_end = str.npos;
+    while (true) {
+        int64_t semicolon = str.find(';');
+        if (comma != 0) {
+            result.push_back(comma == pos_end ? str.substr(0) : str.substr(0, comma));
+        }
+        if (comma == pos_end) {
+            break;
+        }
+        else {
+            str.remove_prefix(comma + 1);
+        }
+    }
+    return result;
+}
